4ced89875dbe1f8ee732293f2584f67e5097eb64
aniruddha <ani@dctacademy.com>
2020-05-15 13:00:17 +0530

    encrypt password

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -1,16 +1,25 @@
 const User = require('../models/user')
+const bcryptjs = require('bcryptjs')
 const usersController = {}
 
 usersController.register = (req, res) => {
     const body = req.body 
     const user = new User(body)
-    user.save()
-        .then((user) =>{
-            res.json(user)
+    bcryptjs.genSalt()
+        .then((salt) => {
+            bcryptjs.hash(user.password, salt)
+                .then((encrypted) => {
+                    user.password = encrypted
+                    user.save()
+                        .then((user) => {
+                            res.json(user)
+                        })
+                        .catch((err) => {
+                            res.json(err)
+                        })
+                })
         })
-        .catch((err) => {
-            res.json(err)
-        })
+   
         
     /*
     const user = new User()
