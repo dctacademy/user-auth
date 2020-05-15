ced5e01059d955f9524404c3a753ecee1e147140
aniruddha <ani@dctacademy.com>
2020-05-15 06:30:24 +0530

    user validation

--- app/models/user.js
+++ app/models/user.js
@@ -3,15 +3,21 @@ const Schema = mongoose.Schema
 const userSchema = new Schema({
     username: {
         type: String, 
-        required: true 
+        required: true, 
+        minlength: 6,
+        maxlength: 64,
+        unique: true
     },
     email: {
         type: String,
-        required: true 
+        required: true,
+        unique: true 
     }, 
     password: {
         type: String, 
-        required: String
+        required: String,
+        minlength: 8,
+        maxlength: 128
     }
 })
 
